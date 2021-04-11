dca0ee9e8852cfdeed50c297ecdcd9a1e077f9a2
duonghuukhoi <duonghuukhoi@gmail.com>
2021-04-11 15:34:09 +0700

    SE and SEM

---
+++ src/SES/StudentEnrolment.java
@@ -0,0 +1,5 @@
+package SES;
+
+public class StudentEnrolment {
+
+}
---
+++ src/SES/StudentEnrolmentManager.java
@@ -0,0 +1,9 @@
+package SES;
+
+public interface StudentEnrolmentManager {
+    public void add();
+    public void update();
+    public void delete();
+    public void getOne();
+    public void getAll();
+}
