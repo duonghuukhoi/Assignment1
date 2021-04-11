ed496a3858ad2beee0267f111d68a4bf8a2fcd6a
duonghuukhoi <duonghuukhoi@gmail.com>
2021-04-11 16:49:56 +0700

    1

--- src/SES/StudentEnrolment.java
+++ src/SES/StudentEnrolment.java
@@ -1,5 +1,59 @@
 package SES;
 
-public class StudentEnrolment {
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
 
+public class StudentEnrolment implements StudentEnrolmentManager{
+
+    @Override
+    public void add() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+
+    @Override
+    public void getOne() {
+
+    }
+
+    @Override
+    public void getAll() {
+
+    }
+    public static void main(String[] args) throws FileNotFoundException{
+        List<List<String>> studentEnrolment = new ArrayList<>();
+        Scanner scanner = new Scanner(new File("default.csv"));
+        while (scanner.hasNext()){
+            String line = scanner.nextLine();
+            String element [] = line.split(",");
+            System.out.println(element[0]);
+        }
+    }
+
+    public static String[][] readFile() throws FileNotFoundException{
+        int count=0;
+        Scanner scanner = new Scanner(new File("default.csv"));
+        List<List<String>> studentEnrolment = new ArrayList<>();
+        while (scanner.hasNext()){
+            String line = scanner.nextLine();
+            String element [] = line.split("");
+            System.out.println(element[0]);
+        }
+        return null;
+    }
 }
